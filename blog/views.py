fa2df2f1248dbd1999fbe442543acb2df08fc031
Rene Cortez <rcortezk9@gmail.com>
2019-07-07 17:21:54 -0500

    -Create Django project -Created Application -Added templates -Bootstrap -CSS

---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'ReneC',
+        'title': 'Blog Post',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
