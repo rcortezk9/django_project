5ab5914cd1b1dd3a6db3f4859d7537c88e1094c2
Rene Cortez <rcortezk9@gmail.com>
2019-07-12 21:43:22 -0500

    -Made edits to img tags -Added AWS variables to AWS v4

--- django_project/settings.py
+++ django_project/settings.py
@@ -140,5 +140,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+S3_USE_SIGV4 = True
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_REGION_NAME = 'us-east-2'
