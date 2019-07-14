14973cb0f48575bc00f68b414166cf7cd61f650d
Rene Cortez <rcortezk9@gmail.com>
2019-07-13 23:51:59 -0500

    -Making changes to the styling

--- blog/views.py
+++ blog/views.py
@@ -15,13 +15,14 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 5
+    paginate_by = 6
 
 class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_posts.html'
     context_object_name = 'posts'
-    paginate_by = 5
+    paginate_by = 6
+
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
